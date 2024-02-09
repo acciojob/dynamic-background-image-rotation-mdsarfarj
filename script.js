91fe0d3fcd4a3a55ef9356baab6a7e020ce70a26
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2024-02-10 00:22:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ function setBackgroundImage() {
+      if (window.innerHeight > window.innerWidth) {
+        document.body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
+      } else {
+        document.body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
+      }
+    }
+
+    // Set background image on page load
+    setBackgroundImage();
+
+    // Set background image on window resize
+    window.onresize = function() {
+      setBackgroundImage();
+    };
